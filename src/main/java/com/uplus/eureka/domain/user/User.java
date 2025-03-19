c61451fce694938314e1886e74f8af1edf5e5916
neungdong <mjkim9503@naver.com>
2025-03-19 17:18:21 +0900

    userDto 분리

--- src/main/java/com/uplus/eureka/domain/user/User.java
+++ src/main/java/com/uplus/eureka/domain/user/User.java
@@ -1,5 +1,6 @@
 package com.uplus.eureka.domain.user;
 
+import com.uplus.eureka.domain.user.dto.SignupRequestDTO;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -17,4 +18,13 @@ public class User {
     private String emailDomain;
     private String joinDate;
     private String refreshToken;
+
+    // SignupRequestDTO를 받아서 User 객체를 생성하는 생성자 추가
+    public User(SignupRequestDTO signupRequestDTO) {
+        this.userId = signupRequestDTO.getUserId();
+        this.userName = signupRequestDTO.getUserName();
+        this.userPwd = signupRequestDTO.getUserPwd();
+        this.emailId = signupRequestDTO.getEmailId();
+        this.emailDomain = signupRequestDTO.getEmailDomain();
+    }
 }
--- src/main/java/com/uplus/eureka/domain/user/service/UserService.java
+++ src/main/java/com/uplus/eureka/domain/user/service/UserService.java
@@ -1,11 +1,13 @@
 package com.uplus.eureka.domain.user.service;
 
-import com.uplus.eureka.domain.user.User;
+import com.uplus.eureka.domain.user.dto.LoginRequestDTO;
+import com.uplus.eureka.domain.user.dto.SignupRequestDTO;
+import com.uplus.eureka.domain.user.dto.UserResponseDTO;
 
 public interface UserService {
-    User login(User user) throws Exception;
-    void signup(User user) throws Exception;
-    User getUserById(String userId) throws Exception;
+    UserResponseDTO login(LoginRequestDTO loginRequestDTO) throws Exception;
+    void signup(SignupRequestDTO signupRequestDTO) throws Exception;
+    UserResponseDTO getUserById(String userId) throws Exception;
     void saveRefreshToken(String userId, String refreshToken) throws Exception;
     String getRefreshToken(String userId) throws Exception;
     void deleteRefreshToken(String userId) throws Exception;
