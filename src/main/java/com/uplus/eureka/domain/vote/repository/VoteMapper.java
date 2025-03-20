9b3a1ca8a7f35ed75ce377635c69c8a1a1398ee2
neungdong <mjkim9503@naver.com>
2025-03-20 20:23:00 +0900

    merge

--- src/main/java/com/uplus/eureka/domain/vote/repository/VoteMapper.java
+++ src/main/java/com/uplus/eureka/domain/vote/repository/VoteMapper.java
@@ -56,34 +56,4 @@ public interface VoteMapper {
     //투표 수정
     void updateVote(@Param("voteId") Integer voteId, @Param("voteRequest") VoteRequest voteRequest);
 
-    // 투표글 ID로 작성자 ID 조회
-    String getVoteCreator(@Param("voteId") Integer voteId);
-
-    /**
-     * 특정 투표글에 참여한 사용자 데이터 삭제
-     */
-    void deleteParticipants(@Param("voteId") Integer voteId);
-
-    /**
-     * 작성자가 맞을 경우 투표 삭제
-     */
-    void deleteVote(@Param("voteId") Integer voteId);
-
-    /**
-     * 특정 투표글 마감 (status 변경)
-     */
-    int closeVote(@Param("voteId") Integer voteId);
-
-    // 투표 생성자(최초 참여자) 조회
-    String getVoteOwner(@Param("voteId") Integer voteId);
-
-    /**
-     * ✅ 마감된 투표 자동 업데이트 (현재 시간과 비교)
-     */
-    int autoCloseVotes();
-
-    /**
-     * 특정 투표글 상태 조회
-     */
-    Vote getVoteStatus(@Param("voteId") Integer voteId);
 }
