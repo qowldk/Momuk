4f27aebf96abea0c15a0d26e13450ab3a60ed7f8
qowldk <qowldk1030@gmail.com>
2025-05-15 15:49:10 +0900

    feat: 투표 참여자 조회 시 userName 포함하도록 응답 개선

--- src/main/java/com/uplus/eureka/domain/participant/Participant.java
+++ src/main/java/com/uplus/eureka/domain/participant/Participant.java
@@ -10,4 +10,5 @@ public class Participant {
     private String userId;        // 사용자 ID (FK)
     private Integer voteId;        // 투표글 ID (FK)
     private LocalDateTime createdAt; // 참여 일자
+    private String userName;
 }
